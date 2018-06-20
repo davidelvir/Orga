8fbf8af5fe4dc876de27306bc5aaf2fd1c65af9d
dmendoza3 <dmendoza3@unitec.edu>
2018-06-19 19:11:51 -0600

    Agregado memoria asociativa

--- src/orga/Memorias.java
+++ src/orga/Memorias.java
@@ -22,7 +22,10 @@ public class Memorias {
     final int conjuntosCache = 16;
     final int tamBloque = 8;
     final int tamConjunto = 4;
+    int lineaCache = 8;
+    int siguiente;
     public int[][][] CACHE_CONJUNTO = new int[16][4][3];
+    int CACHE_A[][] = new int[lineaCache][3];
     public int[][] siguienteConjunto = new int[16][1];
 
     public int k = 8; // tamaño del bloque
@@ -122,7 +125,32 @@ public class Memorias {
         } else if (tipo == 2) {
 
         } else if (tipo == 3) {
+            if (siguiente > lineaCache - 1) {
+                siguiente = 0;
+            }
+            final int bloque = pos / tamBloque;
+            final int etiqueta = bloque;
+            int linea;
 
+            if ((linea = estaEnCacheA(bloque)) != -1) {
+                tiempo += 0.01;
+                CACHE_A[linea][1] = 1;
+                return;
+            }
+            linea = siguiente;
+            if (CACHE_A[linea][0] == -1) {
+                CACHE_A[linea][0] = 1;
+                CACHE_A[linea][2] = etiqueta;
+                tiempo += 0.11;
+            } else {
+                if (CACHE_A[linea][1] == 1) {
+                    tiempo += 0.22;
+                } else {
+                    tiempo += 0.11;
+                }
+                CACHE_A[linea][2] = etiqueta;
+            }
+            siguiente++;
         } else if (tipo == 4) {
             final int bloque = pos / tamBloque;
             final int conjunto = bloque % conjuntosCache;
@@ -162,7 +190,34 @@ public class Memorias {
         } else if (tipo == 2) {
 
         } else if (tipo == 3) {
+            if (siguiente > lineaCache - 1) {
+                siguiente = 0;
+            }
+            final int bloque = pos / tamBloque;
 
+            if (estaEnCacheA(bloque) != -1) {
+                tiempo += 0.01;
+            }
+
+            final int linea = siguiente;
+            final int etiqueta = bloque;
+
+            if (CACHE_A[linea][0] == -1) {
+                CACHE_A[linea][0] = 1;
+                CACHE_A[linea][1] = -1;
+                CACHE_A[linea][2] = etiqueta;
+                siguiente++;
+                tiempo += 0.11;
+            } else {
+                if (CACHE_A[linea][1] == 1) {
+                    CACHE_A[linea][1] = -1;
+                    tiempo += 0.22;
+                } else {
+                    tiempo += 0.11;
+                }
+                CACHE_A[linea][2] = etiqueta;
+                siguiente++;
+            }
         } else if (tipo == 4) {
             final int bloque = pos / tamBloque;
             final int conjunto = bloque % conjuntosCache;
@@ -201,6 +256,15 @@ public class Memorias {
     private int estaEnConjunto(int conjunto, int etiqueta) {
         for (int i = 0; i < tamConjunto; i++) {
             if (CACHE_CONJUNTO[conjunto][i][2] == etiqueta) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    private int estaEnCacheA(int bloque) {
+        for (int i = 0; i < lineaCache; i++) {
+            if (CACHE_A[i][2] == bloque) {
                 return i;
             }
         }
